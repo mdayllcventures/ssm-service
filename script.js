a9d1187f87b2a7930f1694264005614da3ef7c1f
Shina Akinboboye <akinboboye.shina@gmail.com>
2020-08-04 18:56:29 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+const submitButton = document.querySelector('button');
+
+const apiKeyInput = document.querySelector('#api');
+const phoneNumbersInput = document.querySelector('#phone');
+const textMessageInput = document.querySelector('#sms');
+
+const sendSMS = async (apiKey, phoneNumbers, textMessage) => {
+
+    try {
+
+        const sendSMSResponse = await fetch('amazon-url-here', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': apiKey
+            },
+            body: JSON.stringify({
+                textMessage,
+                phoneNumbers
+            })
+        });
+
+        return { data: await sendSMSResponse.json() };
+
+    } catch (error) {
+
+        console.log(error);
+        return { error: 'An error occured, contact Shina!!!' };
+
+    }
+
+}
+
+// register event
+submitButton.addEventListener('click', async function (event) {
+
+    event.preventDefault();
+
+    const apiKey = apiKeyInput.value;
+    const textMessage = textMessageInput.value;
+    let phoneNumbers = phoneNumbersInput.value;
+
+
+    if (apiKey.length === 0 || phoneNumbers.length === 0 || textMessage.length === 0) {
+
+        return alert('apikey, phone numbers and text message must be entered');
+
+    }
+
+    phoneNumbers = phoneNumbersInput.value.replace(/\s/g, '').split(',');
+
+    const apiFeedBack = await sendSMS(apiKey, phoneNumbersInput, textMessage);
+
+    if (apiFeedBack.error) {
+
+        return alert(apiFeedBack.error);
+
+    }
+
+    return alert('Success: SMS successfully sent');
+
+});
+
+
