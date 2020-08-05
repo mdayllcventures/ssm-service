76bc3eaae75bf0576b748afb140beeebdb12d7d9
Shina Akinboboye <akinboboye.shina@gmail.com>
2020-08-04 20:56:14 -0600

    initial commit

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,9 @@
 const submitButton = document.querySelector('button');
 
-const apiKeyInput = document.querySelector('#api');
 const phoneNumbersInput = document.querySelector('#phone');
 const textMessageInput = document.querySelector('#sms');
 
-const sendSMS = async (apiKey, phoneNumbers, textMessage) => {
+const sendSMS = async (phoneNumbers, textMessage) => {
 
     try {
 
@@ -14,13 +13,10 @@ const sendSMS = async (apiKey, phoneNumbers, textMessage) => {
         const body = JSON.stringify({ textMessage, phoneNumbers });
         console.log('the body: ', body);
 
-        const sendSMSResponse = await fetch('https://r21feli8sl.execute-api.us-east-1.amazonaws.com/dev/', {
+        await fetch('https://r21feli8sl.execute-api.us-east-1.amazonaws.com/dev/', {
             method: 'POST',
             mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-api-key': apiKey
-            },
+            headers: { 'Content-Type': 'application/json' },
             body
         });
 
@@ -40,12 +36,11 @@ submitButton.addEventListener('click', async function (event) {
 
     event.preventDefault();
 
-    const apiKey = apiKeyInput.value;
     const textMessage = textMessageInput.value;
     let phoneNumbers = phoneNumbersInput.value;
 
 
-    if (apiKey.length === 0 || phoneNumbers.length === 0 || textMessage.length === 0) {
+    if (phoneNumbers.length === 0 || textMessage.length === 0) {
 
         return alert('apikey, phone numbers and text message must be entered');
 
@@ -53,7 +48,7 @@ submitButton.addEventListener('click', async function (event) {
 
     phoneNumbers = phoneNumbersInput.value.replace(/\s/g, '').split(',').map(number => Number(number));
 
-    const apiFeedBack = await sendSMS(apiKey, phoneNumbers, textMessage);
+    const apiFeedBack = await sendSMS(phoneNumbers, textMessage);
 
     if (apiFeedBack.error) {
 
